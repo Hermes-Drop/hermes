1e957ae2b9208d4827103e78790fcb29fe4b9440
Marlon (Mj) Burog <spicyrice09@gmail.com>
2025-02-01 10:55:01 -0800

    Update servos.py

--- servo_control/servos.py
+++ servo_control/servos.py
@@ -1,1 +1,11 @@
 # handle arduino code here (assuming python)
+# make them functions that are callable
+#
+# 1) convert x,y direction to degrees
+#       degrees is then used to tell arduino to make servos rotate X degrees
+#       assume that any instructions you get will purely be translational not rotational
+#
+# 2) actually sending the degrees into the arduino
+#
+# arduino-python3 0.6
+# pyserial
