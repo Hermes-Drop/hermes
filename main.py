ad3589dfebdd47595c2d27c1013c6bb499003e4d
4-gent <spicyrice09@gmail.com>
2025-03-06 00:03:00 -0800

    adjusted ip address for socket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from camera import OpenCVWebSocket
+from camera_control import OpenCVWebSocket
 
-FLASK_SERVER_URL = "ws://localhost:4000/video"
+FLASK_SERVER_URL = "ws://10.251.4.78:4000/video"
 
 streamer = OpenCVWebSocket(FLASK_SERVER_URL)
 streamer.start()
